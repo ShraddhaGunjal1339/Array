c9d74c8fd5f2c9a184ba33b257bede0b9ab3cdfb
Shraddha <shraddhagunjal1037@gmail.com>
2023-09-13 22:45:16 +0530

    qq

--- code2.cpp
+++ code2.cpp
@@ -1,18 +1,21 @@
-//Take 5 element input in array and print their double
+//Take element input in array and print their double
 
 #include<iostream>
 using namespace std;
 
 int main (){
-    int arr[5];
+    int arr[500];
+    int n;
+    cout << "Give how many elemets you want to insert" << endl;
+    cin >> n;
     cout << "Give input to the array "<< endl;
-    for (int i = 0 ; i < 5 ; i++){
+    for (int i = 0 ; i < n ; i++){
         cin >> arr[i];
     }
 
     cout << "Output is:" << endl;
 
-    for (int i = 0 ; i < 5 ; i++){
+    for (int i = 0 ; i < n ; i++){
         cout << 2*arr[i]<< endl;
     }
     return 0; 
