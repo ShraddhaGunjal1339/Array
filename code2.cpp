81ac9cf43b7f9de2899463e9d7c39f55d880d0c6
Shraddha <shraddhagunjal1037@gmail.com>
2023-09-23 20:11:22 +0530

    a

--- code2.cpp
+++ code2.cpp
@@ -8,6 +8,10 @@ int main (){
     int n;
     cout << "Give how many elemets you want to insert" << endl;
     cin >> n;
+    if(n > 500){
+        cout << "Inseart size less than or equual to 500 "<< endl ;
+        exit(1);
+    }
     cout << "Give input to the array "<< endl;
     for (int i = 0 ; i < n ; i++){
         cin >> arr[i];
