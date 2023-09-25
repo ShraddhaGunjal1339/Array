071fd69de13f83f33ccd5110e760009bafd51be2
Shraddha <shraddhagunjal1037@gmail.com>
2023-09-25 22:48:14 +0530

    a

--- Lecture2/Q3_IntersectionOfTwoArray.cpp
+++ Lecture2/Q3_IntersectionOfTwoArray.cpp
@@ -3,34 +3,26 @@
 
 using namespace std;
 
-int main()
-{
-    vector <int> arr ;
-    arr.push_back(1);
-    arr.push_back(2);
-    arr.push_back(3);
-    arr.push_back(4);
-    arr.push_back(6);
-    arr.push_back(8);
-    
-    vector <int> brr ;
-
-    brr.push_back(3);
-    brr.push_back(4);
-    brr.push_back(9);
-    brr.push_back(10);
-
+void intersection_array(vector<int> arr ,vector<int> brr){
     vector <int> ans;
-
     //Intersection
-
+    //not works for duplicate elements without adding marking condition 
+    //if you add mark it works for duplicate also
     for (int i = 0; i < arr.size(); i++)
     {
         int ele =arr[i];
         for (int j = 0; j < brr.size(); j++)
         {
             if(ele == brr[j]){
-                ans.push_back(brr[j]);
+                //to handel duplicate elements
+                //mark as already added to ans
+                //-->make that element =-1(works for +ve ele array)
+                brr[j]=-1;    //IMP
+
+                //if array contain both +ve and -ve number mark using =INT_MIN
+
+                
+                ans.push_back(ele);
             }
         }
         
@@ -73,5 +65,53 @@ int main()
     }
     cout << endl;
 
+
+}
+//O/P -->7  5  5  5  5  5  4  2  2  2  2  
+
+int main()
+{
+    vector <int> arr ;
+    // arr.push_back(1);
+    // arr.push_back(2);
+    // arr.push_back(3);
+    // arr.push_back(3);
+    // arr.push_back(4);
+    // arr.push_back(6);
+    // arr.push_back(8);
+
+    arr.push_back(7);
+    arr.push_back(6);
+    arr.push_back(5);
+    arr.push_back(5);
+    arr.push_back(5);
+    arr.push_back(4);
+    arr.push_back(2);
+    
+    vector <int> brr ;
+
+    // brr.push_back(3);
+    // arr.push_back(3);
+    // brr.push_back(4);
+    // brr.push_back(9);
+    // brr.push_back(10);
+
+    brr.push_back(1);
+    brr.push_back(2);
+    brr.push_back(3);
+    brr.push_back(4);
+    brr.push_back(5);
+    brr.push_back(5);
+    brr.push_back(5);
+    brr.push_back(5);
+    brr.push_back(5);
+    brr.push_back(2);
+    brr.push_back(2);
+    brr.push_back(2);
+    brr.push_back(7);
+    brr.push_back(1);
+
+    
+    intersection_array(arr,brr);
     return 0;
 }
