2351d2ca7c5db9de1b1e663899a48c7d62421c65
Shraddha <shraddhagunjal1037@gmail.com>
2023-09-27 21:19:52 +0530

    AA

--- Lecture2/Q8_DutchNaturalFlag.cpp
+++ Lecture2/Q8_DutchNaturalFlag.cpp
@@ -49,29 +49,16 @@ int main()
 
     dutch_natural_flag(arr);
 
-    //  int start = 0;
-    // int end = arr . size() -1 ;
-    // int i = 0;
-    // // for(int index = 0 ; index < arr . size() ; index++){
-    //     if(arr[i] == 0){
-    //         swap(arr[i],arr[start]);
-    //         i++;
-    //         start++;
-    //     }
-    //     if(arr[i] == 1){
-    //         swap(arr[i],arr[end]);
-    //         i++;
-    //         end++;
-    //     }
-    
-    
-    // for(auto ele : ans){
-    //     cout << ele <<"  ";
-    // }
-    for (int  i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << "  ";
+
+    //print the arr
+    for(auto ele : arr){
+        cout << ele <<"  ";
     }
+    // OR use 
+    // for (int  i = 0; i < arr.size(); i++)
+    // {
+    //     cout << arr[i] << "  ";
+    // }
     
     cout << endl;
     return 0;
